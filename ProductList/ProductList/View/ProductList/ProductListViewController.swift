cf80519d271f0ed911c55bc34a3a2b5f462ec600
onur.basdas <onurebasdase@hotmail.com>
2024-02-22 19:48:29 +0300

    Code design completed.

--- ProductList/ProductList/View/ProductList/ProductListViewController.swift
+++ ProductList/ProductList/View/ProductList/ProductListViewController.swift
@@ -16,10 +16,7 @@ class ProductListViewController: UIViewController {
     @IBOutlet weak var productListSortBtn: UIButton!
     
     let viewModel = ProductListViewModel()
-    var products: [Product] = []
-    var filteredProducts: [Product] = []
     var isAscendingOrder = true
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,28 +26,32 @@ class ProductListViewController: UIViewController {
     private func setupUI() {
         productListTableView.dataSource = self
         productListTableView.delegate = self
-        productListTableView.register(ProductListTableViewCell.nib(), forCellReuseIdentifier: ProductListTableViewCell.identifier)
         productListSearchBar.delegate = self
+        productListTableView.register(ProductListTableViewCell.nib(), forCellReuseIdentifier: ProductListTableViewCell.identifier)
         configureButton()
         fetchData()
     }
     
     private func configureLabel() {
-        let text = "Ürünler (Toplam \(products.count) adet)"
+        let text = "Ürünler (Toplam \(viewModel.filteredProducts.count) adet)"
         let attributedString = NSMutableAttributedString(string: text)
+        
         let boldAttributes: [NSAttributedString.Key: Any] = [
             .font: UIFont.boldSystemFont(ofSize: 17),
             .foregroundColor: UIColor.black
         ]
         attributedString.addAttributes(boldAttributes, range: (text as NSString).range(of: "Ürünler"))
+        
         let normalAttributes: [NSAttributedString.Key: Any] = [
             .font: UIFont.systemFont(ofSize: 17),
             .foregroundColor: UIColor.gray
         ]
         
-        attributedString.addAttributes(normalAttributes, range: NSRange(location: 0, length: text.count))
+        attributedString.addAttributes(normalAttributes, range: NSRange(location: "Ürünler".count, length: text.count - "Ürünler".count))
+        
         productListTitleLabel.attributedText = attributedString
     }
+
     
     private func configureButton() {
         productListFilterBtn.layer.borderWidth = 1
@@ -74,8 +75,8 @@ class ProductListViewController: UIViewController {
     
     func handleSuccess(productList: ProductListModel) {
         if let products = productList.products {
-            self.products = products
-            self.filteredProducts = products
+            self.viewModel.products = products
+            self.viewModel.filteredProducts = products
             DispatchQueue.main.async { [weak self] in
                 self?.configureLabel()
                 self?.productListTableView.reloadData()
@@ -92,53 +93,34 @@ class ProductListViewController: UIViewController {
     }
     
     @IBAction func productListSortBtnPressed(_ sender: UIButton) {
-        isAscendingOrder = !isAscendingOrder
-        let sortedProducts = products.sorted { (product1, product2) -> Bool in
-            if isAscendingOrder {
-                return (product1.price ?? 0) < (product2.price ?? 0)
-            } else {
-                return (product1.price ?? 0) > (product2.price ?? 0)
-            }
-        }
-        updateUI(with: sortedProducts)
-    }
-    
-    func updateUI(with sortedProducts: [Product]) {
-        self.filteredProducts = sortedProducts
-        self.productListTableView.reloadData()
+        viewModel.isAscendingOrder.toggle()
+        viewModel.sortProducts()
+        productListTableView.reloadData()
     }
 }
 
 extension ProductListViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredProducts.count
+        return viewModel.filteredProducts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ProductListTableViewCell.identifier, for: indexPath) as! ProductListTableViewCell
-        cell.bind(data: filteredProducts[indexPath.row])
+        cell.bind(data: viewModel.filteredProducts[indexPath.row])
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let selectedProductVC = storyboard?.instantiateViewController(withIdentifier: "ProductListDetailViewController") as! ProductListDetailViewController
-        selectedProductVC.selectedProduct = filteredProducts[indexPath.row]
+        selectedProductVC.selectedProduct = viewModel.filteredProducts[indexPath.row]
         navigationController?.pushViewController(selectedProductVC, animated: true)
     }
 }
 
 extension ProductListViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText.isEmpty {
-            filteredProducts = products
-        } else {
-            filteredProducts = products.filter {
-                if let name = $0.title {
-                    return name.lowercased().contains(searchText.lowercased())
-                }
-                return false
-            }
-        }
+        viewModel.searchProducts(searchText: searchText)
         productListTableView.reloadData()
+        self.configureLabel()
     }
 }
--- ProductList/ProductList/ViewModel/ProductListViewModel.swift
+++ ProductList/ProductList/ViewModel/ProductListViewModel.swift
@@ -9,6 +9,34 @@ import Foundation
 
 class ProductListViewModel {
     
+    var products: [Product] = []
+    var filteredProducts: [Product] = []
+    var isAscendingOrder = true
+    
+    func sortProducts() {
+         let sortedProducts = products.sorted { (product1, product2) -> Bool in
+             if isAscendingOrder {
+                 return (product1.price ?? 0) < (product2.price ?? 0)
+             } else {
+                 return (product1.price ?? 0) > (product2.price ?? 0)
+             }
+         }
+         filteredProducts = sortedProducts
+     }
+    
+    func searchProducts(searchText: String) {
+           if searchText.isEmpty {
+               filteredProducts = products
+           } else {
+               filteredProducts = products.filter {
+                   if let name = $0.title {
+                       return name.lowercased().contains(searchText.lowercased())
+                   }
+                   return false
+               }
+           }
+       }
+    
     func fetchProductList(completion: @escaping (Result<ProductListModel, NetworkError>) -> Void) {
         let urlString = Constants.apiURL
         NetworkManager.shared.fetchData(from: urlString, method: .get) { (result: Result<ProductListModel, NetworkError>) in
